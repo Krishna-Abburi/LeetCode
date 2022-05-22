eec5ece3c13dd99afca5e1c75bd7e4090db16287
Chinni Krishna Abburi <102870588+Krishna-Abburi@users.noreply.github.com>
2022-05-22 13:47:01 -0400

    Time: 444 ms (88.56%), Space: 0B (100.00%) - LeetHub

---
+++ 1747-leetflex-banned-accounts/1747-leetflex-banned-accounts.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+
+select distinct b.account_id from loginfo a join loginfo b on a.account_id=b.account_id and a.ip_address!=b.ip_address where b.login between a.login and a.logout
