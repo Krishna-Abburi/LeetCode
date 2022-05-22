b622d989648255307b5473a3ba598908e1f77468
Chinni Krishna Abburi <102870588+Krishna-Abburi@users.noreply.github.com>
2022-05-22 12:22:12 -0400

    Time: 629 ms (54.12%), Space: 0B (100.00%) - LeetHub

--- 1988-find-cutoff-score-for-each-school/1988-find-cutoff-score-for-each-school.sql
+++ 1988-find-cutoff-score-for-each-school/1988-find-cutoff-score-for-each-school.sql
@@ -1,3 +1,3 @@
-select school_id,ifnull(min(score),-1) as score from schools a left join exam b on capacity>=student_count group by school_id
+select school_id,ifnull(min(score),-1) as score  from schools a left join exam b on a.capacity>=b.student_count group by school_id
 
 
