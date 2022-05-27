2d1f4234769974e543252c45eb7866e7ec1706a4
Chinni Krishna Abburi <102870588+Krishna-Abburi@users.noreply.github.com>
2022-05-27 16:59:19 -0400

    Time: 655 ms (43.46%), Space: 0B (100.00%) - LeetHub

---
+++ 1565-unique-orders-and-customers-per-month/1565-unique-orders-and-customers-per-month.sql
@@ -0,0 +1,1 @@
+select date_format(order_date,'%Y-%m') as month,count(distinct order_id) as order_count,count(distinct customer_id) as customer_count from orders where invoice>20 group by  date_format(order_date,'%Y-%m')
