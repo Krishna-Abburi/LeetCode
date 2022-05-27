3e73ac04d2d20c442212b8c857884b8264093226
Chinni Krishna Abburi <102870588+Krishna-Abburi@users.noreply.github.com>
2022-05-26 23:14:41 -0400

    Time: 442 ms (68.71%), Space: 0B (100.00%) - LeetHub

---
+++ 1142-user-activity-for-the-past-30-days-ii/1142-user-activity-for-the-past-30-days-ii.sql
@@ -0,0 +1,1 @@
+select ifnull(round(sum(a)/count(user_id),2),0) as average_sessions_per_user from (select user_id,count(distinct session_id) as a from activity where datediff('2019-07-27',activity_date)<30 group by user_id) m
