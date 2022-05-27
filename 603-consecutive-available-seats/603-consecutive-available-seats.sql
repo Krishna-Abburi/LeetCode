223fb54b49f8c4eac3fb74483f51c62d261703d4
Chinni Krishna Abburi <102870588+Krishna-Abburi@users.noreply.github.com>
2022-05-26 20:41:41 -0400

    Time: 770 ms (5.02%), Space: 0B (100.00%) - LeetHub

--- 603-consecutive-available-seats/603-consecutive-available-seats.sql
+++ 603-consecutive-available-seats/603-consecutive-available-seats.sql
@@ -1,4 +1,3 @@
-select seat_id from (
-select * from (select seat_id,free,lag(free,1)over() as a from cinema) m where free=a and free=1
+select * from (select seat_id from (select seat_id,free,lag(free,1) over() as a from cinema) m where free=a and free=1
 union
-select * from (select seat_id,free,lead(free,1)over() as a from cinema) n where free=a and free=1) t  order by seat_id
+select seat_id from (select seat_id,free,lead(free,1) over() as a from cinema) m where free=a and free=1) t order by seat_id
