42c5c3982594e95b8707bdbf6e0c8a4fbf6b39c8
Chinni Krishna Abburi <102870588+Krishna-Abburi@users.noreply.github.com>
2022-05-30 13:33:13 -0400

    Time: 512 ms (95.35%), Space: 0B (100.00%) - LeetHub

---
+++ 2205-the-number-of-users-that-are-eligible-for-discount/2205-the-number-of-users-that-are-eligible-for-discount.sql
@@ -0,0 +1,6 @@
+CREATE FUNCTION getUserIDs(startDate DATE, endDate DATE, minAmount INT) RETURNS INT
+BEGIN
+  RETURN (
+select count(distinct user_id) as user_count from purchases where (time_stamp between startDate and endDate) and amount>=minamount  
+  );
+END
